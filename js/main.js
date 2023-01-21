87f566190aec78dbdfb3878afbb27c905467d5dc
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-01-21 17:18:42 -0300

    Agrega funciones para mostrar y ocultar objetos

--- js/main.js
+++ js/main.js
@@ -37,3 +37,27 @@ function borrarIntegrantesAnteriores() {
     }
 }
 
+function mostrarBotonCalcular() {
+    document.querySelector('button[name=calcular]').classList.remove('oculto');
+}
+
+function ocultarBotonCalcular() {
+    document.querySelector('button[name=calcular]').classList.add('oculto');
+}
+
+function mostrarRespuestas() {
+    document.querySelector('#respuestas').className = '';
+}
+
+function ocultarRespuestas() {
+    document.querySelector('#respuestas').className = 'oculto';
+}
+
+function mostrarBotonReiniciar() {
+    document.querySelector('button[name=reiniciar]').classList.remove('oculto');
+}
+
+function ocultarBotonReiniciar() {
+    document.querySelector('button[name=reiniciar]').classList.add('oculto');
+}
+
